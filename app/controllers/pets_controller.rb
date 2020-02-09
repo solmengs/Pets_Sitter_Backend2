63bfcc71e1492eb201fa1ff08a1fa88c245013d0
Alice Won <alicewon4@gmail.com>
2020-02-09 13:41:52 -0800

    updated controllers

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,2 +1,32 @@
 class PetsController < ApplicationController
+  def index
+    pets = Pet.all
+    render json: pets
+  end
+
+  def show
+      pet = Pet.where(pet_id: params[:id])
+      render json: pet
+  end
+
+  def create
+      pet = Pet.new(params.require(:pet).permit(:name, :image, :anmial_type, :age))
+      if pet.save
+          render json: pet   
+      else
+          flash[:message] = pet.errors.full_messages
+      end    
+  end
+
+  def update
+      pet = Pet.find(params[:id])
+      pet.update(params.require(:pet).permit(:name, :image, :anmial_type, :age))
+      render json: pet
+  end
+
+  def destroy
+      pet = Pet.find(params[:id])
+      pet.destroy
+      render json: {message: 'Your pet has been removed!'}
+  end
 end
